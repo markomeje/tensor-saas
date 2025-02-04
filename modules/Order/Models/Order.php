3ae24ffdb0a6aef19901e9bb79527bd3bab69b95
markomeje <markomejeonline@gmail.com>
2025-02-04 09:03:22 +0100

    Modules setup

---
+++ modules/Order/Models/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Order\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
